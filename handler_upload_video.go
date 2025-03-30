96f22850ea273b7502092a47d11b7063755bc8a4
Damien Nelson <dhnisaman@gmail.com>
2025-03-30 01:09:43 -0400

    organize videos uploaded to s3 bucket by aspect ratio

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -84,6 +87,19 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "failed to move data", err)
 		return
 	}
+	
+	aspRatio, err := getVideoAspectRatio(tmpFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failed to get ratio", err)
+	}
+	prefix := ""
+	if aspRatio == "16:9"{
+		prefix = "landscape"
+	} else if aspRatio == "9:16"{
+		prefix = "portrait"
+	}else{
+		prefix = "other"
+	}
 
 	randBytes := make([]byte, 32)
 	_, err = rand.Read(randBytes)
@@ -92,7 +108,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 
 	fileName := base64.RawURLEncoding.EncodeToString(randBytes)
-	nameWithExt := fmt.Sprintf("%s.%s", fileName, ext)
+	nameWithExt := fmt.Sprintf("%s/%s.%s", prefix, fileName, ext)
 
 	_ , err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket: &cfg.s3Bucket,
@@ -113,3 +129,41 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	
 	respondWithJSON(w, http.StatusOK, videoData)
 }
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	props := struct {
+		Streams []struct {
+			Width  int `json:"width"`
+			Height int `json:"height"`
+		} `json:"streams"`
+	}{}
+
+	if err = json.Unmarshal(out.Bytes(), &props); err != nil {
+		return "", err
+	}
+	if len(props.Streams) == 0 {
+		return "", fmt.Errorf("no streams found in video")
+	}
+
+	width := props.Streams[0].Width
+	height := props.Streams[0].Height
+	ratio := float64(width) / float64(height)
+	const tolerance = 0.01
+	if ratio > (16.0/9.0-tolerance) && ratio < (16.0/9.0+tolerance) {
+		return "16:9", nil
+	}
+	if ratio > (9.0/16.0-tolerance) && ratio < (9.0/16.0+tolerance) {
+		return "9:16", nil
+	}
+
+	return "other", nil
+}
+
